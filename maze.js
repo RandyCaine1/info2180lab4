f5df4ab25b31c9d1636dd9a009d903e16ac7bbc1
RandyCaine1 <nicoycaine1@gmail.com>
2017-10-20 18:46:45 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -3,10 +3,24 @@ var boundry;
 var hit;
 
 window.onload = function(){
-	$("boundary1").onmouseover = border;
-}
+	//$("boundary1").onmouseover = border;
+	//Exercise 1
+	var boundaries = document.querySelectorAll("");
 
-var border = function(){
-	$("boundary1").addClassName("youlose")
-	boundry = $$(".boundary");
-}
+	var changeBounds = function(){
+		for(var i = 0; i < boundaries.length;i++){
+			boundaries[i].className += "youlose";
+		}
+	}
+
+	for(var i=0; i < boundaries.length;i++){
+		boundaries[i].onmouseover = function(){
+			changeBounds();
+		};
+	}
+};
+
+//var border = function(){
+	//$("boundary1").addClassName("youlose")
+	//boundry = $$(".boundary");
+//};
